71b567e804bdc2504f7cb90da8248355f1559816
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-12-27 01:21:36 +0530

    Update Logo image source

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -10,7 +10,7 @@
 	/>
 
 	<img
-		src="https://wqz50k0spm0gyalr.public.blob.vercel-storage.com/IMG-20231209-WA0010-qAHQIWONOBY0h1IrwpbaMyHUevhn0I.jpg"
+		src="https://wqz50k0spm0gyalr.public.blob.vercel-storage.com/IMG-20231209-WA0010-ophLQTNOdwP7maPYYp16CTgUjMcaWp.jpg"
 		class="custom-logo-opacity me-3 hidden h-16 sm:h-8 dark:block"
 		alt="Dark Logo"
 	/>
