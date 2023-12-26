deb941f0163f3c0287fbde473018e02a95022d12
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-12-27 01:26:17 +0530

    Update Logo component styling

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -5,13 +5,13 @@
 <NavBrand href="/">
 	<img
 		src="https://wqz50k0spm0gyalr.public.blob.vercel-storage.com/IMG-20231226-WA0003-MdiT3WBgM0ojEtdRx9QMJj2Sr6Jfei.jpg"
-		class=" me-3 block h-12 sm:h-16 dark:hidden"
+		class=" me-3 block h-10 sm:h-8 dark:hidden"
 		alt="White Logo"
 	/>
 
 	<img
 		src="https://wqz50k0spm0gyalr.public.blob.vercel-storage.com/IMG-20231209-WA0010-ophLQTNOdwP7maPYYp16CTgUjMcaWp.jpg"
-		class="custom-logo-opacity me-3 hidden h-16 sm:h-8 dark:block"
+		class="custom-logo-opacity me-3 hidden h-10 sm:h-8 dark:block"
 		alt="Dark Logo"
 	/>
 </NavBrand>
