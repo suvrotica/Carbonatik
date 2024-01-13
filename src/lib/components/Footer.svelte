b52c84cacef665d69df0928fad6e9b58d7a496ad
suvrowebthree <suvro.ghosh.contact@gmail.com>
2024-01-13 09:16:49 +0530

    Update Footer margin

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
 	import Logo from './Logo.svelte';
 </script>
 
-<footer class="mt-16 bg-white p-4 sm:p-6 dark:bg-neutral-900">
+<footer class="mt-1 bg-white p-4 sm:p-6 dark:bg-neutral-900">
 	<div class="mx-auto max-w-screen-xl">
 		<div class="sm:flex sm:items-center sm:justify-between">
 			<span class="text-sm text-neutral-500 sm:text-center dark:text-neutral-400"
