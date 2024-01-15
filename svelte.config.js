7c2d81058d74932da2a2589cba858274e4b8baa9
suvrowebthree <suvro.ghosh.contact@gmail.com>
2024-01-15 07:21:28 +0530

    Add prerender error handling in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,18 @@ const config = {
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
+	},
+
+	prerender: {
+		handleHttpError: ({ path, referrer, message }) => {
+			// ignore deliberate link to shiny 404 page
+			if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+				return;
+			}
+
+			// otherwise fail the build
+			throw new Error(message);
+		}
 	}
 };
 
