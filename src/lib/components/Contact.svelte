931c380d32608955e1eb464ae0fe198af5759309
suvrowebthree <suvro.ghosh.contact@gmail.com>
2024-01-15 13:44:04 +0530

    Update font styles and section background in app.pcss and Contact.svelte

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,20 +1,13 @@
 <script lang="ts">
 	import GoldLine from '$lib/components/GoldLine.svelte';
-
-	const sectionStyle =
-		'bg-neutral-200 bg-contain bg-repeat bg-blend-soft-light light-bg dark:bg-neutral-950 dark:bg-blend-overlay dark:dark-bg3';
 </script>
 
 <GoldLine />
 
-<section id="ContactUs" class={sectionStyle}>
-	<h2
-		class="classico-bold text-2xl font-extrabold tracking-tight text-neutral-900 dark:text-neutral-100"
-	>
-		Contact Us
-	</h2>
+<section id="ContactUs">
+	<h2>Contact Us</h2>
 
-	<p class="lara-light text-base text-neutral-600 dark:text-neutral-300">
+	<p>
 		Let’s discuss your real estate aspirations. Contact us for more information on our properties,
 		investment opportunities, or to schedule a consultation.
 	</p>
