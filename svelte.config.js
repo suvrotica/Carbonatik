8443a37cde2a69b97276ea83a433bb6ed798d032
suvrowebthree <suvro.ghosh.contact@gmail.com>
2024-01-15 07:25:04 +0530

    Update handleHttpError function in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -16,15 +16,7 @@ const config = {
 	},
 
 	prerender: {
-		handleHttpError: ({ path, referrer, message }) => {
-			// ignore deliberate link to shiny 404 page
-			if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
-				return;
-			}
-
-			// otherwise fail the build
-			throw new Error(message);
-		}
+		handleMissingId: 'warn'
 	}
 };
 
