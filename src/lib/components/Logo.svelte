f5d96e71e10a1f213fb2b7ac4d9e5882a2eefac5
suvrowebthree <suvro.ghosh.contact@gmail.com>
2024-01-17 09:49:01 +0530

    Update Logo component styling

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,13 +1,13 @@
 <a href="/">
 	<img
 		src="https://wqz50k0spm0gyalr.public.blob.vercel-storage.com/IMG-20231226-WA0003-MdiT3WBgM0ojEtdRx9QMJj2Sr6Jfei.jpg"
-		class=" inline h-8 rounded-none dark:hidden"
+		class=" inline h-14 rounded-none dark:hidden"
 		alt="White Logo"
 	/>
 
 	<img
 		src="https://wqz50k0spm0gyalr.public.blob.vercel-storage.com/IMG-20231209-WA0010-ophLQTNOdwP7maPYYp16CTgUjMcaWp.jpg"
-		class=" hidden h-8 rounded-none dark:inline"
+		class=" hidden h-14 rounded-none dark:inline"
 		alt="Dark Logo"
 	/>
 </a>
