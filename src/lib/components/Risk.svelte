cb25f078da9a5ee44c01e60b126dbdbf6734486e
suvrowebthree <suvro.ghosh.contact@gmail.com>
2024-01-27 18:16:19 +0530

    Update Risk component heading and subheading

--- src/lib/components/Risk.svelte
+++ src/lib/components/Risk.svelte
@@ -3,8 +3,8 @@
 	import HeroTemplate from '$lib/components/HeroTemplate.svelte';
 
 	let quoteProps = {
-		heading: 'Managing Risk',
-		subheading: 'OPTIMIZED',
+		heading: 'Risk',
+		subheading: 'MINIIMIZED',
 		content: `We recognize that effective risk management is pivotal in safeguarding our clients' assets and maximizing their investment potential. Our comprehensive risk management strategies encompass thorough market analysis, thorough due diligence, project feasibility studies, and ongoing monitoring of operational and market risks. By continually assessing and managing risks, we strive to protect and enhance the value of our portfolio while delivering steady and reliable returns.`,
 		image: imgURL14
 	};
