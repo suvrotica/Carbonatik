472b3062f1f61d7a2c6e98834f8ce7dce56bf29f
suvrotica <117360356+suvrotica@users.noreply.github.com>
2024-02-06 02:49:03 +0530

    Update Brendan.svelte

--- src/lib/components/Brendan.svelte
+++ src/lib/components/Brendan.svelte
@@ -4,7 +4,7 @@
 
 	let quoteProps = {
 		heading: 'Brendan Fitzpatrick ( BF ) ',
-		subheading: 'CO-FOUNDER',
+		subheading: 'FOUNDER',
 		content: `Brendan Fitzpatrick, co-founder of BFJK Corp, stands as a beacon of innovation and excellence in the real estate asset management industry. With a rich background in real estate finance and development, Brendan has been a key architect in building BFJK's prestigious portfolio. His expertise lies in leveraging financial acumen and market insight to drive growth and maximize asset value.
 
 Brendan's strategic leadership has been crucial in navigating the complexities of the real estate market, consistently delivering results that exceed investor expectations. His forward-thinking approach has led BFJK Corp to embrace technological advancements, ensuring efficiency and competitiveness in a rapidly evolving industry.
